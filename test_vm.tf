c854d8cff52cade70fb2ac86f99d3900ed115f7e
eddimull <eddimull@gmail.com>
2023-09-30 13:25:54 -0500

    going to the bare essentials

--- test_vm.tf
+++ test_vm.tf
@@ -1,27 +1,27 @@
 resource "proxmox_vm_qemu" "test_vm" {
   name        = "tftest1.xyz.com"
   desc        = "tf description"
-  target_node = "proxmox1-xx"
+  target_node = "proxmox"
 
   clone = "ci-ubuntu-template"
 
   # The destination resource pool for the new VM
-  pool = "pool0"
+  # pool = "pool0"
 
-  disk {
-    slot    = 0
-    type    = "scsi"
-    storage = "local-lvm"
-    size    = "10G"
-  }
+  # disk {
+  #   slot    = 0
+  #   type    = "scsi"
+  #   storage = "local-lvm"
+  #   size    = "10G"
+  # }
 
-  cores   = 3
-  sockets = 1
-  memory  = 1024
+  # cores   = 3
+  # sockets = 1
+  # memory  = 1024
 
-  network {
-    model  = "virtio"
-    bridge = "vmbr0"
-  }
-  os_type = "cloud-init"
+  # network {
+  #   model  = "virtio"
+  #   bridge = "vmbr0"
+  # }
+  # os_type = "6.x - 2.6 Kernel"
 }
