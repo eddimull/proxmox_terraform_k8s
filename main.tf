68874dfee1371c38ba380943221807fad854c6e8
eddimull <eddimull@gmail.com>
2023-09-30 12:39:52 -0500

    adding debugging

--- main.tf
+++ main.tf
@@ -11,6 +11,7 @@ provider "proxmox" {
   pm_api_url          = var.proxmox_api_url
   pm_api_token_id     = var.proxmox_token
   pm_api_token_secret = var.proxmox_secret
+  pm_debug            = true
 }
 
 variable "proxmox_api_url" {}
