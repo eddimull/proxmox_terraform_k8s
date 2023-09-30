c3ec56b28c49a6cc5c715e10602020143a977d85
eddimull <eddimull@gmail.com>
2023-09-30 14:51:29 -0500

    remove debugging

--- main.tf
+++ main.tf
@@ -11,11 +11,11 @@ provider "proxmox" {
   pm_api_url          = var.proxmox_api_url
   pm_api_token_id     = var.proxmox_token
   pm_api_token_secret = var.proxmox_secret
-  pm_debug            = true
-  pm_log_levels = {
-    _default    = "debug"
-    _capturelog = ""
-  }
+  # pm_debug            = true
+  # pm_log_levels = {
+  #   _default    = "debug"
+  #   _capturelog = ""
+  # }
 }
 
 variable "proxmox_api_url" {}
