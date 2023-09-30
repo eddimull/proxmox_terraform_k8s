db6de2a12f5d2d385a8333282f97c628c5bd5491
eddimull <eddimull@gmail.com>
2023-09-30 14:50:00 -0500

    remove debugging

--- main.tf
+++ main.tf
@@ -11,8 +11,6 @@ provider "proxmox" {
   pm_api_url          = var.proxmox_api_url
   pm_api_token_id     = var.proxmox_token
   pm_api_token_secret = var.proxmox_secret
-  pm_log_enable       = true
-  pm_log_file         = "terraform-plugin-proxmox.log"
   pm_debug            = true
   pm_log_levels = {
     _default    = "debug"
